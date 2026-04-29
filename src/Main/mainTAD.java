9163620d3ec31c6b0e42462d823ea7e2757d8a09
zDefcon21 <antholozadarequelme@gmail.com>
2026-04-28 20:21:29 -0500

    Update / main TAD
    se agrego el insertar elemento al tadList

--- src/Main/mainTAD.java
+++ src/Main/mainTAD.java
@@ -1,9 +1,68 @@
 package Main;
 
+import java.util.Scanner;
+
 import TADArrayList.TADArrList;
 
 public class mainTAD {
     public static void main(String[] args) {
     	TADArrList<Integer> TADList = new TADArrList<Integer>("Lista De Nombres"); 
+    	Scanner input = new Scanner(System.in);
+    	int n = 0, elemento = 0;
+    	boolean on = false;
+    	do {
+    	System.out.println("---------------------------------------------------");
+    	System.out.println("                   TAD ArrayList                   ");
+    	System.out.println("---------------------------------------------------");
+    	System.out.println("---------------------------------------------------");
+    	System.out.println("Cuantos elementos enteros desea agregar a la lista?");
+    	System.out.println("---------------------------------------------------");
+    	if(!input.hasNextInt()) {
+			System.out.println("---------------------------------------------------");
+			System.out.println("       Error: Ingrese un numero entero valido      ");
+			System.out.println("---------------------------------------------------");
+			input.nextLine();
+			on = false;
+    	}else {
+    		n = input.nextInt();
+    		if(n <= 0) {
+    			System.out.println("---------------------------------------------------");
+				System.out.println("       Error: Ingrese un numero entero Positivo    ");
+				System.out.println("---------------------------------------------------");
+				input.nextLine();
+				on = false;
+    		}else {
+    			on = true;
+    		}
+    	}
+    	}while(!on); // mientras on sea falso, se repetira
+    	input.nextLine();
+    	System.out.println("---------------------------------------------------");
+    	System.out.println("Ingrese los elementos enteros a agregar a la lista");
+    	System.out.println("---------------------------------------------------");
+    	for (int i = 0; i < n; i++) {
+			System.out.print("Elemento ["+i+"]: ");
+			if(!input.hasNextInt()) {
+				System.out.println("---------------------------------------------------");
+				System.out.println("       Error: Ingrese un numero entero valido      ");
+				System.out.println("---------------------------------------------------");
+				input.nextLine();
+				i--;
+	    	}else {
+	    		elemento = input.nextInt();
+	    		if(elemento <= 0) {
+	    			System.out.println("---------------------------------------------------");
+					System.out.println("       Error: Ingrese un numero entero Positivo    ");
+					System.out.println("---------------------------------------------------");
+					input.nextLine();
+					i--;
+	    		}else {
+	    			TADList.insertarElemento(elemento); // se inserta elementos en el arrayList
+	    			input.nextLine();
+	    		}
+	    	}
+			
+		}
+    	
     }
 }
